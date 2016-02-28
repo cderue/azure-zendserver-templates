fd09357834260ba20933a41ae21237e456908ab6
Cédric Derue <cedric.derue@gmail.com>
2016-02-28 17:43:45 +0100

    Update bootstrapZs.sh

--- bootstrapZs.sh
+++ bootstrapZs.sh
@@ -9,7 +9,7 @@ zend_admin_email='cedric.derue@gmail.com'
 ## Bootstrap and create	or join	cluster.
 sudo /usr/local/zend/bin/zs-manage bootstrap-single-server -p "$zendadmin_ui_pass" -r TRUE -a TRUE -e "$zend_admin_email" -d "$zenddev_ui_pass"
 ## Restart Zend	Server
-#sudo /usr/local/zend/bin/zendctl.sh	restart
+sudo /usr/local/zend/bin/zendctl.sh restart
 #web_api_key=`sqlite3 /usr/local/zend/var/db/gui.db "select	HASH from GUI_WEBAPI_KEYS where NAME='admin';"`
 #sudo /usr/local/zend/bin/zs-manage restart -N admin -K "$web_api_key"
 #$ command -v foo > /dev/null 2>&1 || { echo >&2 "I require foo but it's not installed. Aborting."; exit 1; }
